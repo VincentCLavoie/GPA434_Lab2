55937e78439e0ab8d5f78d4b55209ca9bdbb04f8
Liliam Elena Acosta <liliam_elena@hotmail.com>
2022-11-09 21:09:24 -0500

    3e fonction temperature fonctionne

--- QSmartHouse/CustomAnnualWeather.cpp
+++ QSmartHouse/CustomAnnualWeather.cpp
@@ -7,5 +7,19 @@ QString CustomAnnualWeather::name() const
 
 double CustomAnnualWeather::temperature(size_t secondOfYear) const 
 {
-	return 30.0;
+	double tmensuel, tsaisonal, x, y;
+	const double tmmax = 30.0,
+				 tmmin = 1.0,
+				 thmax = 5.0,
+				 thmin = 2.0;
+	const int tannee = 31536000;
+	
+
+	x = 0.5 * (sin((secondOfYear %tannee)/5019110) )+ 0.5; //0.5 * (sin((secondOfYear %tannee * 2 * 3.1416) / (tmois * 12)) )+ 0.5; où tmois = 2628000
+	y = 0.5 * (sin((secondOfYear %tannee)/13751) )+ 0.5; //0.5 * (sin((secondOfYear %tanne * 2 * 3.1416) / (theure * 24)) )+ 0.5; où theure = 3600
+
+	tmensuel = (tmmax - tmmin) * x + tmmin;
+	tsaisonal = (thmax - thmin) * y + thmin;
+
+	return tmensuel + tsaisonal;
 }
